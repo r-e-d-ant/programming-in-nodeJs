1d87a449278b216d099638e9bd1055a38a4b5632
r-e-d-ant <mugishathierry65@gmail.com>
2022-08-04 20:15:42 +0200

    requests and responses

--- intermediate/server.js
+++ intermediate/server.js
@@ -1,8 +1,34 @@
 
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer(() => {
-    console.log('request made');
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    /*
+    // set header content type
+    res.setHeader('Content-Type', 'Text/plain');
+    res.write('hello, Mugisha');
+    */
+
+    /*
+    // set header content type
+    res.setHeader('Content-Type', 'Text/html');
+    res.write('<h1>hello, Mugisha Thierry</h1>');
+    res.write('<h1>hello again, Mugisha Thierry</h1>');
+    */
+   
+    // set header content type
+    res.setHeader('Content-Type', 'Text/html');
+    // sent an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    })
 });
 
 server.listen(3000, 'localhost', () => {
