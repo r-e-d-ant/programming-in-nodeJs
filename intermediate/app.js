7acf662f53f549447da59799d885152a23b99949
r-e-d-ant <mugishathierry65@gmail.com>
2022-08-05 21:20:16 +0200

    EJS

--- intermediate/app.js
+++ intermediate/app.js
@@ -4,24 +4,34 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    // res.render('./views/index.html', { root: __dirname }); // old before we add ejs view engine
+    res.render('index');
 })
 app.get('/about', (req, res) => {
     // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    // res.sendFile('./views/about.html', { root: __dirname }); // old before we add ejs view engine
+    res.render('about')
+})
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
 })
 
 // redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-})
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// })
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    // res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 })
