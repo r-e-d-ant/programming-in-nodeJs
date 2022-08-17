c9d96bae6b91d8bf865249364f0bd927dbfb9560
r-e-d-ant <mugishathierry65@gmail.com>
2022-08-17 23:02:05 +0200

    Get, Post and Delete Requests

--- intermediate/app.js
+++ intermediate/app.js
@@ -22,19 +22,23 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // routes
 app.get('/', (req, res) => {
     res.redirect('/blogs');
-})
+});
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' })
-})
+});
 
-// blog routes
+// == blog routes ==
+
+// (get blogs from db)
 app.get('/blogs', (req, res) => {
+    // find all blogs and render it to the index route
     Blog.find().sort({ createdAt: -1 })
         .then((result) => {
             res.render('index', { title: 'All Blogs', blogs: result })
@@ -42,12 +46,52 @@ app.get('/blogs', (req, res) => {
         .catch((err) => {
             console.log(err);
         })
-})
+});
+
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create' });
-})
+});
+
+// post blog to db
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+    
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.log(err.message);
+        })
+});
+
+// get one post
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    Blog.findById(id)
+        .then((result) => {
+            res.render('details', { blog: result , title: `Blog | ${result.title}`});
+        })
+        .catch((err) => {
+            console.log(err.message);
+        })
+});
+
+// handler for a delete request
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs' });
+        })
+        .catch(err => {
+            console.log(err.message);
+        })
+});
 
 // 404 page
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
-})
+});
